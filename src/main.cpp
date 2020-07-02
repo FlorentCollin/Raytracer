782964c749793ddc93cfb472ab99134a30d62f9b
FlorentCollin <florent.collin23@gmail.com>
2020-07-03 00:59:17 +0200

    Chapter 4 : Visualisation of a clock using matrices transformations
    Visualisation of a clock using only the matrices transformations defined
    in the chapter 4 (translation, scaling, rotation).
    Note: This implementation is just to show how theses transformations can
    work and this file does not implement it in the best way possible.
    It was just coded as a proof of concept.

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,43 @@
 #include <math.h>
 
 #include <iostream>
+#include <vector>
 
+#include "transformations.h"
 #include "canvas.h"
-#include "tuple.h"
 
-struct Projectile {
-    Point position;
-    Vector velocity;
-    Projectile(Point t_position, Vector t_velocity)
-        : position{t_position}, velocity{t_velocity} {};
-};
+using namespace transformations;
 
-struct Environment {
-    Vector gravity, wind;
-    Environment(Vector t_gravity, Vector t_wind)
-        : gravity{t_gravity}, wind{t_wind} {};
-};
-
-Projectile tick(Environment env, Projectile proj) {
-    Point position = proj.position + proj.velocity;
-    Vector velocity = proj.velocity + env.gravity + env.wind;
-    return Projectile(position, velocity);
+void print_point(Point p) {
+    std::cout << "(" << p.x() << ", " << p.y() << ", " << p.z() << ")\n";
 }
 
 int main() {
-    Vector velocity(1, 1.8, 0);
-    velocity.normalize();
-    velocity = velocity * 11.25;
-    Projectile proj(Point(0, 1, 0), velocity);
-    Environment env(Vector(0, -0.1, 0), Vector(-0.01, 0, 0));
-    Canvas canvas(900, 500);
-    Color c(1, 0.5, 0);
-    for (int i = 0; proj.position.y() >= 0; i++) {
-        int x = static_cast<int>(std::round(proj.position.x()));
-        int y = static_cast<int>(canvas.get_height() - 1 - std::round(proj.position.y()));
-        canvas.write_pixel(x, y, c);
-        proj = tick(env, proj);
+    const int width = 100;
+    const int height = 100;
+    Canvas canvas(width, height);
+    Color point_color(1, 0, 0);
+    auto translation_center = translation(width/2, height/2, 0);
+    Point start(0, 0, 0);
+    std::vector<Point> v({Point(0, 1, 0), Point(1, 0, 0), Point(0, -1, 0), Point(-1, 0, 0)});
+    auto scale = scaling(20, 20, 0);
+    for(Point p : v) {
+        p = scale * p;
+        p = translation_center * p;
+        canvas.write_pixel(p.x(), p.y(), point_color);
     }
-    std::cout << canvas.to_PPM() << '\n';
+    for(Point p : v) {
+        p = rotation_z(M_PI/6) * p;
+        p = scale * p;
+        p = translation_center * p;
+        canvas.write_pixel(p.x(), p.y(), point_color);
+    }
+    for(Point p : v) {
+        p = rotation_z(M_PI/3) * p;
+        p = scale * p;
+        p = translation_center * p;
+        canvas.write_pixel(p.x(), p.y(), point_color);
+    }
+
+    std::cout << canvas.to_PPM();
 }
